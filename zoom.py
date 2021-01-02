26a2b5b59bfd3c8bf886d6c088b354922b50fcf9
elan00001 <elan00001@gmail.com>
2021-01-02 23:44:38 +0800

    Update zoom.py

---
+++ zoom.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+infile = 'D:\\original_img.jpg'
+outfile = 'D:\\adjust_img.jpg'
+im = Image.open(infile)
+(x,y) = im.size #read image size
+x_s = 250 #define standard width
+y_s = y * x_s / x #calc height based on standard width
+out = im.resize((x_s,y_s),Image.ANTIALIAS) #resize image with high-quality
+out.save(outfile)
+
+im2 = Image.open(outfile)
