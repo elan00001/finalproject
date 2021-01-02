aa5f459a84d95eb8de7caf6599610984abee0cd8
JRSmel0For3 <72684274+JRSmel0For3@users.noreply.github.com>
2021-01-02 23:54:03 +0800

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -4,7 +4,7 @@ infile = 'D:\\original_img.jpg'
 outfile = 'D:\\adjust_img.jpg'
 im = Image.open(infile)
 (x,y) = im.size #read image size
-x_s = 250 #define standard width
+x_s = 512 #define standard width
 y_s = y * x_s / x #calc height based on standard width
 out = im.resize((x_s,y_s),Image.ANTIALIAS) #resize image with high-quality
 out.save(outfile)
